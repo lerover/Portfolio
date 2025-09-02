c3d4beb84317250ef243761186abf2ef865d5fae
Win Pyae <lerover1155@gmail.com>
2025-09-02 19:04:11 +0630

    add css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Resume from "./resume";
 import Testimonial from "./testimonial";
 import PopularClients from "./popular-clients";
 import ContactForm from "./contact-form";
+import "@/globals.css";
 
 export default function Portfolio() {
   return (
