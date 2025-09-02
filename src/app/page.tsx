a1d282d932d35e015b03ba5f5bab63f49379ccdc
Win Pyae <lerover1155@gmail.com>
2025-09-02 19:31:03 +0630

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import Resume from "./resume";
 import Testimonial from "./testimonial";
 import PopularClients from "./popular-clients";
 import ContactForm from "./contact-form";
-import "@/globals.css";
 
 export default function Portfolio() {
   return (
