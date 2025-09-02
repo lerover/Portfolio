8ba430accb6f933e7d00d462a2bcbe01c2bb9a82
Win Pyae <lerover1155@gmail.com>
2025-09-02 19:25:36 +0630

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === "production";
  
 const nextConfig = {
-  basePath: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template" : "",
-  assetPrefix: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template/" : "",
+  // basePath: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template" : "",
+  // assetPrefix: isProd ? "/Next.js-Tailwind-CSS-Portfolio-Template/" : "",
   output: "export",
   images: {
     unoptimized: true,
