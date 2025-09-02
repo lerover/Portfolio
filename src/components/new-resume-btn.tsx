fddde9f470fd650a4322d58795f4f944e619c408
Win Pyae <lerover1155@gmail.com>
2025-09-02 16:27:06 +0630

    Make pdfdownloadlink only render in client side

--- src/components/new-resume-btn.tsx
+++ src/components/new-resume-btn.tsx
@@ -1,47 +1,33 @@
-// components/ResumeButton.tsx
 "use client";
-import { PDFDownloadLink } from "@react-pdf/renderer";
+import dynamic from "next/dynamic";
 import MyDocument from "./new-resume";
 import { ArrowDownTrayIcon } from "@heroicons/react/24/solid";
 import { Button } from "@material-tailwind/react";
 
+// Dynamically load PDFDownloadLink only on client
+const PDFDownloadLink = dynamic(
+  () => import('@react-pdf/renderer').then(mod => mod.PDFDownloadLink),
+  { ssr: false }
+);
+
 export function NewResumeButton() {
- return(
+  return (
     <PDFDownloadLink document={<MyDocument />} fileName="resume.pdf">
-    {({ loading }) => (
-      <Button variant="text" color="gray" className="flex items-center gap-2">
-        {loading ?
-         ("Generating..." )
-         : (
+      {({ loading }) => (
+        <Button variant="text" color="gray" className="flex items-center gap-2">
+          {loading ? "Generating..." : (
             <>
-            Download My Resume
-
-            <ArrowDownTrayIcon
-            strokeWidth={3}
-            className="h-3.5 w-3.5 text-gray-900"
-            />
+              Download My Resume
+              <ArrowDownTrayIcon
+                strokeWidth={3}
+                className="h-3.5 w-3.5 text-gray-900"
+              />
             </>
-         )}
-        
-      </Button>
-    )}
-  </PDFDownloadLink>
-
-
-
- )
+          )}
+        </Button>
+      )}
+    </PDFDownloadLink>
+  );
 }
 
 export default NewResumeButton;
-
-/* <Button
-variant="text"
-color="gray"
-className="flex items-center gap-2"
->
-Download My Resume
-<ArrowDownTrayIcon
-  strokeWidth={3}
-  className="h-3.5 w-3.5 text-gray-900"
-/>
-</Button> */
